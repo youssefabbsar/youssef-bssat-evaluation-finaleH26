a3408c10d5179566855a7a4bac372e39489a4403
youssefabbsar <youssefabbsar@gmail.com>
2026-06-17 10:09:17 -0400

    Update API endpoint for fetching livres

--- bibliotheque/client/src/pages/Livres.jsx
+++ bibliotheque/client/src/pages/Livres.jsx
@@ -5,7 +5,7 @@ function Livres() {
   const [livres, setLivres] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/livres").then((res) => {
+    axios.get("https://youssef-bssat-evaluation-finaleh26.onrender.com/api/livres").then((res) => {
       setLivres(res.data);
     });
   }, []);
